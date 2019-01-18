84ffa1ad7593da116c9ffefb9fdfe76f445d0997
Julian <hello@juliantrueflynn.com>
2019-01-18 14:39:56 -0500

    Bar validations and tests

--- app/models/bar.rb
+++ app/models/bar.rb
@@ -1,3 +1,18 @@
 class Bar < ApplicationRecord
   belongs_to :shop
+
+  validates_presence_of :title,
+    :content,
+    :position,
+    :location,
+    :text_color,
+    :bg_color,
+    :template_enabled
+  
+  validates_inclusion_of :position, in: %w(fixed static)
+  validates_inclusion_of :location, in: %w(top bottom)
+  validates_inclusion_of :is_new_tab_url, in: [true, false]
+  validates_inclusion_of :has_close_button, in: [true, false]
+  validates_inclusion_of :template_enabled,
+    in: %w(global homepage collection product)
 end
--- spec/models/bar_spec.rb
+++ spec/models/bar_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Bar, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { expect(FactoryBot.build(:bar)).to be_valid }
+
+  it { is_expected.to validate_presence_of :title }
+  it { is_expected.to validate_presence_of :content }
+  it { is_expected.to validate_presence_of :bg_color }
+  it { is_expected.to validate_presence_of :template_enabled }
+
+  it { is_expected.to_not allow_value(nil).for(:content)}
+  it { is_expected.to_not allow_value(nil).for(:is_new_tab_url) }
+  it { is_expected.to_not allow_value(nil).for(:has_close_button) }
+  it { is_expected.to_not allow_value(nil).for(:has_close_button) }
+
+  it { is_expected.to validate_length_of(:content).is_at_least(0) }
+
+  it { is_expected.to validate_inclusion_of(:position).in_array(%w(fixed static)) }
+  it { is_expected.to validate_inclusion_of(:location).in_array(%w(top bottom)) }
+  it { is_expected.to validate_inclusion_of(:template_enabled).in_array(%w(global homepage collection product)) }
 end
