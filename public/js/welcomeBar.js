ce9351a383e0a8c1d83923a12e34b50e2cdfc174
Julian <hello@juliantrueflynn.com>
2019-01-19 16:14:33 -0500

    Init and inject stylesheet

--- public/js/welcomeBar.js
+++ public/js/welcomeBar.js
@@ -1,9 +1,21 @@
 const { shop } = window.Shopify || {};
+const apiHostname = 'conditionalwelcomebar.ngrok.io';
 console.log(shop);
-// TODO: import welcomeBar stylesheet
+
+appendStylesheet(`https://${apiHostname}/css/welcomeBar.css`);
 
 // TODO: Model: fetch rails api
 
 // TODO: Controller: set conditional vars for view
 
 // TODO: View: set HTML from controller
+
+function appendStylesheet(hrefPathname) {
+  const head = document.getElementsByTagName('head')[0];
+  const link = document.createElement('link');
+  link.rel = 'stylesheet';
+  link.type = 'text/css';
+  link.href = hrefPathname;
+
+  head.appendChild(link);
+}
