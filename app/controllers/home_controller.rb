b01777467a58c7d2386b3ac471fc1f01d161e69e
Julian <hello@juliantrueflynn.com>
2019-01-19 13:35:47 -0500

    Set api scope and remove read_products scope

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,5 @@
 class HomeController < ShopifyApp::AuthenticatedController
   def index
-    @products = ShopifyAPI::Product.find(:all, params: { limit: 10 })
     @webhooks = ShopifyAPI::Webhook.find(:all)
   end
 end
--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -2,8 +2,7 @@ ShopifyApp.configure do |config|
   config.application_name = "Conditional Welcome Bar"
   config.api_key = ENV['SHOPIFY_CLIENT_API_KEY']
   config.secret = ENV['SHOPIFY_CLIENT_API_SECRET']
-  config.scope = "read_products" # Consult this page for more scope options:
-                                 # https://help.shopify.com/en/api/getting-started/authentication/oauth/scopes
+  config.scope = 'read_script_tags, write_script_tags'
   config.embedded_app = true
   config.after_authenticate_job = false
   config.session_repository = Shop
