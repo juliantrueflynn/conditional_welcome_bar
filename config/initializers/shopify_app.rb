3b8995db7ab875206a13303a8442339212005e7c
Julian <hello@juliantrueflynn.com>
2019-01-19 20:19:46 -0500

    Remove read_script_tags scope

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -2,7 +2,7 @@ ShopifyApp.configure do |config|
   config.application_name = "Conditional Welcome Bar"
   config.api_key = ENV['SHOPIFY_CLIENT_API_KEY']
   config.secret = ENV['SHOPIFY_CLIENT_API_SECRET']
-  config.scope = 'read_script_tags, write_script_tags'
+  config.scope = 'write_script_tags'
   config.embedded_app = true
   config.after_authenticate_job = false
   config.session_repository = Shop
