160718119f97da6f4af3699b4e91cc7c9c46db6f
Julian <hello@juliantrueflynn.com>
2019-01-20 00:05:12 -0500

    Add destroy api response and test

--- app/controllers/api/bars_controller.rb
+++ app/controllers/api/bars_controller.rb
@@ -1,4 +1,6 @@
 class Api::BarsController < ApplicationController
+  before_action :set_bar, only: [:show, :update, :destroy]
+
   def index
     @bars = Bar.with_domain_name(params[:hostname])
   end
@@ -10,9 +12,18 @@ class Api::BarsController < ApplicationController
   end
 
   def destroy
+    if @bar && @bar.destroy
+      render json: @bar
+    else
+      render json: ['not found']
+    end
   end
 
   private
+
+  def set_bar
+    @bar = Bar.find_by(id: params[:id])
+  end
 
   def bar_params
     params.require(:bar).permit(:id, :hostname)
--- spec/requests/bars_spec.rb
+++ spec/requests/bars_spec.rb
@@ -26,4 +26,13 @@ RSpec.describe 'Bars', type: :request do
       expect(response).to have_http_status(200)
     end
   end
+
+  describe "DELETE /bar/:id" do
+    let!(:bar) { FactoryBot.create(:bar) }
+
+    it 'change count' do
+      destroy_api_url = "#{ENV['API_URL']}/api/bars/#{bar.id}"
+      expect { delete destroy_api_url }.to change { Bar.count }.by(-1)
+    end
+  end
 end
