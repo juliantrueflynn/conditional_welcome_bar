ed3835bf8a8e9e3382404a6f61264d5d3163bceb
Julian <hello@juliantrueflynn.com>
2019-01-20 15:11:21 -0500

    Create default bar after create shop callback

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -7,4 +7,12 @@ class Shop < ActiveRecord::Base
     shopify_domain = "#{hostname}.myshopify.com"
     find_by_shopify_domain(shopify_domain)
   end
+
+  after_create_commit :generate_default_bar
+
+  private
+
+  def generate_default_bar
+    bars.create(title: 'First welcome bar!')
+  end
 end
--- spec/models/shop_spec.rb
+++ spec/models/shop_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
+require 'support/shopify_test_helpers'
 
 RSpec.describe Shop, type: :model do
   it { is_expected.to have_many(:bars) }
+
+  describe '#save' do
+    let!(:shop) { FactoryBot.create(:shop) }
+
+    it 'create default bar' do
+      expect(shop.bars.length).to eq(1)
+    end
+  end
 end
