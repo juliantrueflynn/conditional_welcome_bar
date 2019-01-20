b6acb1aa8d6c4d5e7ea8e4e306cea8e7950d760b
Julian <hello@juliantrueflynn.com>
2019-01-19 19:49:37 -0500

    Init insert welcomeBar HTML

--- public/js/welcomeBar.js
+++ public/js/welcomeBar.js
@@ -1,14 +1,23 @@
 const { shop } = window.Shopify || {};
 const apiHostname = 'conditionalwelcomebar.ngrok.io';
-console.log(shop);
 
 appendStylesheet(`https://${apiHostname}/css/welcomeBar.css`);
+
+insertWelcomeBar();
 
 // TODO: Model: fetch rails api
 
 // TODO: Controller: set conditional vars for view
 
 // TODO: View: set HTML from controller
+
+function insertWelcomeBar() {
+  const body = document.getElementsByTagName('body')[0];
+  const bar = document.createElement('div');
+  bar.classList.add('cw-bar');
+  bar.innerHTML = 'WELCOME BAR HERE';
+  body.insertBefore(bar, body.firstChild);
+}
 
 function appendStylesheet(hrefPathname) {
   const head = document.getElementsByTagName('head')[0];
