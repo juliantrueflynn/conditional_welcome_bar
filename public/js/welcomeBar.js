c15d5a0a49b7a49a829dfb21c5c861a419123b27
Julian <hello@juliantrueflynn.com>
2019-01-20 19:17:49 -0500

    Refactor js to better api structure

--- public/js/welcomeBar.js
+++ public/js/welcomeBar.js
@@ -1,72 +1,77 @@
-const { shop } = window.Shopify || {};
-const apiHostname = 'conditionalwelcomebar.ngrok.io';
-const apiUrl = `https://${apiHostname}/api`;
-const shopDomain = shop.replace('.myshopify.com', '');
 
-appendStylesheet(`//${apiHostname}/css/welcomeBar.css`);
+(function () {
+  'use strict';
 
-insertWelcomeBar();
+  const API_HOSTNAME = 'conditionalwelcomebar.ngrok.io';
+  const API_URL = `https://${API_HOSTNAME}`;
 
-fetchBarsIndex();
+  const api = {
+    init: function () {
+      api.appendStylesheet(`//${API_HOSTNAME}/css/welcomeBar.css`);
+      api.fetchBarsIndex();
+    },
+    fetchBarsIndex: function (apiPath) {
+      const xhr = new XMLHttpRequest();
+      const { shop } = window.Shopify || {};
+      const shopDomain = shop.replace('.myshopify.com', '');
+    
+      xhr.onreadystatechange = function() {
+        if (this.readyState === 4 && this.status === 200) {
+          const response = JSON.parse(this.responseText);
+          const bar = api.getBarFromResponse(response);
+          api.insertWelcomeBar(bar);
+        }
+      };
 
-function getCurrentTemplate() {
-  const path = window.location.pathname;
-  let template;
+      xhr.open('GET', `${API_URL}/api/shops/${shopDomain}/active_bars`);
+      xhr.send();
+    },
+    getBarFromResponse: function (response) {
+      const template = api.getCurrentTemplate();
+    
+      const bars = response.filter(({ templateEnabled }) => (
+        templateEnabled === 'global' || templateEnabled === template
+      ));
+    
+      console.log(bars);
+      console.log(bars[0]);
 
-  if (path === '/') {
-    template = 'homepage';
-  } else if (path === '/cart') {
-    template = 'cart';
-  } else if (path.indexOf('/products/') !== -1) {
-    template = 'product';
-  } else if (path.indexOf('/collections/') !== -1) {
-    template = 'collection';
-  }
+      return bars[0];
+    },
+    appendStylesheet: function (hrefPathname) {
+      const head = document.getElementsByTagName('head')[0];
+      const link = document.createElement('link');
+      link.rel = 'stylesheet';
+      link.type = 'text/css';
+      link.href = hrefPathname;
+    
+      head.appendChild(link);
+    },
+    getCurrentTemplate: function () {
+      let template;
 
-  return template;
-}
-
-function fetchBarsIndex() {
-  const request = new XMLHttpRequest();
-
-  request.onreadystatechange = function() {
-    if (this.readyState === 4 && this.status === 200) {
-      const response = JSON.parse(this.responseText);
-      const bar = getBarFromResponse(response);
+      if (window.location.pathname === '/') {
+        template = 'homepage';
+      } else if (window.location.pathname === '/cart') {
+        template = 'cart';
+      } else if (window.location.pathname.indexOf('/products/') !== -1) {
+        template = 'product';
+      } else if (window.location.pathname.indexOf('/collections/') !== -1) {
+        template = 'collection';
+      }
+    
+      return template;
+    },
+    insertWelcomeBar: function () {
+      const body = document.getElementsByTagName('body')[0];
+      const bar = document.createElement('div');
+      bar.classList.add('cw-bar');
+      bar.innerHTML = 'WELCOME BAR HERE';
+      body.insertBefore(bar, body.firstChild);
     }
   };
 
-  request.open('GET', `${apiUrl}/shops/${shopDomain}/active_bars`);
-  request.send();
-}
+  window.welcomeBarAPI = api;
 
-function getBarFromResponse(response) {
-  const template = getCurrentTemplate();
-
-  const bars = response.filter(({ templateEnabled }) => (
-    templateEnabled === 'global' || templateEnabled === template
-  ));
-
-  console.log(bars);
-  console.log(bars[0]);
-
-  return bars[0];
-}
-
-function insertWelcomeBar() {
-  const body = document.getElementsByTagName('body')[0];
-  const bar = document.createElement('div');
-  bar.classList.add('cw-bar');
-  bar.innerHTML = 'WELCOME BAR HERE';
-  body.insertBefore(bar, body.firstChild);
-}
-
-function appendStylesheet(hrefPathname) {
-  const head = document.getElementsByTagName('head')[0];
-  const link = document.createElement('link');
-  link.rel = 'stylesheet';
-  link.type = 'text/css';
-  link.href = hrefPathname;
-
-  head.appendChild(link);
-}
+  api.init();
+})();
