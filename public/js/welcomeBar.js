b024344c54ffefb1dfdfca82b7d2477bad34fdf9
Julian <hello@juliantrueflynn.com>
2019-01-22 13:39:55 -0500

    Move logic to createElement function

--- public/js/welcomeBar.js
+++ public/js/welcomeBar.js
@@ -5,6 +5,51 @@
   const API_HOSTNAME = 'conditionalwelcomebar.ngrok.io';
   const API_URL = `https://${API_HOSTNAME}`;
 
+  function createElement(selector, props) {
+    const elem = document.createElement(selector);
+
+    Object.keys(props).filter(key => props[key]).forEach((key) => {
+      if (key === 'style') {
+        style(elem, props.style);
+      } else if (key === 'classList') {
+        classList(elem, props.classList);
+      } else {
+        elem[key] = props[key];
+      }
+    });
+
+    return elem;
+  }
+
+  function style(node, styles) {
+    return styles && Object.keys(styles)
+      .filter(key => styles[key]).forEach((key) => {
+        node.style[key] = styles[key];
+      });
+  }
+
+  function classList(node, classNames) {
+    return classNames && classNames.forEach((className) => {
+      node.classList.add(className);
+    });
+  }
+
+  function getCurrentTemplate() {
+    let template;
+
+    if (window.location.pathname === '/') {
+      template = 'homepage';
+    } else if (window.location.pathname === '/cart') {
+      template = 'cart';
+    } else if (window.location.pathname.indexOf('/products/') !== -1) {
+      template = 'product';
+    } else if (window.location.pathname.indexOf('/collections/') !== -1) {
+      template = 'collection';
+    }
+  
+    return template;
+  }
+
   const api = {
     bar: {},
     init: function () {
@@ -34,7 +79,7 @@
     },
     getBarFromResponse: function (response) {
       const bars = response.filter(function (bar) {
-        const template = api.getCurrentTemplate();
+        const template = getCurrentTemplate();
         const isHidden = window.localStorage.getItem(api.getStorageKey(bar.id));
 
         return (
@@ -52,105 +97,77 @@
       link.href = `//${API_HOSTNAME}/css/welcomeBar.css`;
       document.head.appendChild(link);
     },
-    getCurrentTemplate: function () {
-      let template;
-
-      if (window.location.pathname === '/') {
-        template = 'homepage';
-      } else if (window.location.pathname === '/cart') {
-        template = 'cart';
-      } else if (window.location.pathname.indexOf('/products/') !== -1) {
-        template = 'product';
-      } else if (window.location.pathname.indexOf('/collections/') !== -1) {
-        template = 'collection';
-      }
-    
-      return template;
-    },
     getStorageKey: function (barId) {
-      return `cw_bar_hide_${barId}_${api.getCurrentTemplate()}`;
+      return `cw_bar_hide_${barId}_${getCurrentTemplate()}`;
     },
     handleCloseClick: function (e) {
       const container = e.target.parentElement.parentElement.parentElement;
       container.style.display = 'none';
       window.localStorage.setItem(api.getStorageKey(api.bar.id), true);
     },
-    style: function (node, style) {
-      return Object.keys(style).filter(key => style[key]).forEach((key) => {
-        node.style[key] = style[key];
-      });
-    },
-    classList: function (node, classNames) {
-      return classNames.forEach((className) => {
-        node.classList.add(className);
-      });
-    },
     render: function (props) {
-      const container = document.createElement('div');
-      container.id = `cwBar${props.id}`;
-
-      api.classList(container, [
-        'cw-bar',
-        `cw-bar__template-${api.getCurrentTemplate()}`,
-        props.isSticky && 'cw-bar__fixed',
-        props.isSticky && `cw-bar__fixed--${props.placement}`,
-        props.backgroundImage && 'cw-bar__imageable',
-        props.url && 'cw-bar__linkable',
-        props.url && props.isFullWidthLink && 'cw-bar__linkable--w-100',
-        props.url && !props.isFullWidthLink && 'cw-bar__linkable--w-auto',
-      ]);
-
-      const backgroundImage = props.backgroundImage;
+      const image = props.backgroundImage;
       
       let imageSize = props.backgroundImageSizeX;
       if (props.backgroundImageSizeY) {
         imageSize += ` ${props.backgroundImageSizeY}`;
       }
 
-      api.style(container, {
-        fontSize: props.fontSize,
-        color: props.fontColor,
-        textAlign: props.textAlign,
-        backgroundColor: props.backgroundColor,
-        backgroundImage: backgroundImage && `url(${props.backgroundImage})`,
-        backgroundRepeat: backgroundImage && props.backgroundImageRepeat,
-        backgroundPositionX: backgroundImage && props.backgroundImagePositionX,
-        backgroundPositionY: backgroundImage && props.backgroundImagePositionY,
-        backgroundSize: backgroundImage && imageSize,
+      const container = createElement('div', {
+        id: `cwBar${props.id}`,
+        classList: [
+          'cw-bar',
+          `cw-bar__template-${getCurrentTemplate()}`,
+          props.isSticky && 'cw-bar__fixed',
+          props.isSticky && `cw-bar__fixed--${props.placement}`,
+          props.backgroundImage && 'cw-bar__imageable',
+          props.url && 'cw-bar__linkable',
+          props.url && props.isFullWidthLink && 'cw-bar__linkable--w-100',
+          props.url && !props.isFullWidthLink && 'cw-bar__linkable--w-auto',
+        ],
+        style: {
+          fontSize: props.fontSize,
+          color: props.fontColor,
+          textAlign: props.textAlign,
+          backgroundColor: props.backgroundColor,
+          backgroundImage: image && `url(${props.backgroundImage})`,
+          backgroundRepeat: image && props.backgroundImageRepeat,
+          backgroundPositionX: image && props.backgroundImagePositionX,
+          backgroundPositionY: image && props.backgroundImagePositionY,
+          backgroundSize: image && imageSize,
+        }
       });
 
-      let content = document.createElement('div');
+      let contentElSelector = props.url ? 'a' : 'div';
+      const contentWrapper = createElement(contentElSelector, {
+        style: { padding: `${props.paddingY} ${props.paddingX}` },
+        classList: ['cw-bar__content'],
+        href: props.url,
+        target: props.isNewTablUrl,
+        innerHTML: props.content,
+      });
 
-      if (props.url) {
-        content = document.createElement('a');
-        content.href = props.url;
-      }
+      contentWrapper.appendChild(props.content);
 
-      if (props.url && props.isNewTablUrl) {
-        content.target = '_blank';
-      }
+      const row = createElement('div', {
+        classList: ['cw-bar__row'],
+      });
 
-      api.classList(content, ['cw-bar__content']);
-      api.style(content, { padding: `${props.paddingY} ${props.paddingX}` });
-
-      content.innerHTML = props.content;
-
-      const row = document.createElement('div');
-      api.classList(row, ['cw-bar__row']);
-
-      row.appendChild(content);
+      row.appendChild(contentWrapper);
 
       if (props.hasCloseButton) {
-        const buttonCloseWrapper = document.createElement('div');
-        const buttonClose = document.createElement('button');
+        const buttonCloseWrapper = createElement('div', {
+          classList: ['cw-bar__close'],
+          style: { paddingRight: props.paddingX },
+        });
 
-        buttonClose.setAttribute('aria-label', 'Close bar');
-        buttonClose.type = 'button';
-        buttonClose.innerHTML = '&times;';
+        const buttonClose = createElement('button', {
+          type: 'button',
+          ariaLabel: 'Close bar',
+          innerText: '\u00D7',
+        });
+
         buttonClose.addEventListener('click', api.handleCloseClick);
-
-        api.classList(buttonCloseWrapper, ['cw-bar__close']);
-        api.style(buttonCloseWrapper, { paddingRight: props.paddingX });
         buttonCloseWrapper.appendChild(buttonClose);
         row.appendChild(buttonCloseWrapper);
       }
@@ -160,7 +177,7 @@
 
       if (props.isSticky) {
         const height = container.getBoundingClientRect().height;
-        document.body.setAttribute('style', `margin-top: ${height}px`);
+        document.body.style.marginTop = `${height}px`;
       }
     }
   };
