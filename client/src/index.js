d2111d2f9d35f46166def3da602cf48ed5e116ac
Julian <hello@juliantrueflynn.com>
2019-01-22 16:31:33 -0500

    Demo Polaris sample

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import '@shopify/polaris/styles.css';
+import { AppProvider } from '@shopify/polaris';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from './App';
+
+ReactDOM.render(
+  <AppProvider>
+    <App />
+  </AppProvider>,
+  document.getElementById('root'),
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
