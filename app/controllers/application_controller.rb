68d480a03700f1248c4151f437f8925d53aa39f6
Julian <hello@juliantrueflynn.com>
2019-01-28 17:37:14 -0500

    CSRF skip for json requests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,29 +1,3 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate 
-
-  def logged_in?
-    !!current_shop
-  end
-
-  def current_shop
-    @shop ||= Shop.find(auth['shop']) if auth_present?
-  end
-
-  def authenticate
-    render json: ['unauthorized'], status: 404 unless logged_in?
-  end
-
-  private
-
-  def token
-    request.env['HTTP_AUTHORIZATION'].scan(/Bearer (.*)$/).flatten.last
-  end
-
-  def auth
-    Auth.decode(token)
-  end
-
-  def auth_present?
-    !!request.env.fetch('HTTP_AUTHORIZATION', '').scan(/Bearer/).flatten.first
-  end
+  protect_from_forgery unless: -> { request.format.json? }
 end
