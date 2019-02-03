68a24208c66cbce5eb0739446398c5574e9cb663
Julian <hello@juliantrueflynn.com>
2019-02-02 21:43:24 -0500

    Split form fields to separate component

---
+++ client/components/SingleBarFormFields/index.js
@@ -0,0 +1,293 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {
+  Layout,
+  FormLayout,
+  Card,
+  TextField,
+  ChoiceList,
+  Checkbox,
+  ColorPicker,
+  RangeSlider,
+} from '@shopify/polaris';
+
+const SingleBarFormFields = ({
+  updateFieldValue,
+  updateFieldWithPixel,
+  updateColorPickerValue,
+  ...fieldValues
+}) => {
+  const {
+    title,
+    content,
+    hasCloseButton,
+    isActive,
+    placement,
+    pageTemplate,
+    isSticky,
+    url,
+    isFullWidthLink,
+    isNewTabUrl,
+    paddingY,
+    paddingX,
+    fontSize,
+    fontColor,
+    fontFamily, // @TODO: Google fonts select?
+    textAlign,
+    backgroundColor,
+    backgroundImage, // @TODO: Image uploader
+    backgroundImageRepeat,
+    backgroundImageSizeY,
+    backgroundImageSizeX,
+    backgroundImagePositionX,
+    backgroundImagePositionY,
+  } = fieldValues;
+
+  return (
+    <Layout>
+      <Layout.AnnotatedSection title="Editor" description="Edit welcome bar title or content.">
+        <Card sectioned>
+          <FormLayout>
+            <TextField
+              value={title}
+              id="title"
+              label="Title"
+              placeholder="Title"
+              onChange={updateFieldValue}
+            />
+            <TextField
+              value={content}
+              label="Content"
+              id="content"
+              placeholder="Start typing..."
+              onChange={updateFieldValue}
+              multiline
+            />
+          </FormLayout>
+        </Card>
+      </Layout.AnnotatedSection>
+      <Layout.AnnotatedSection
+        title="Visibility"
+        description="Set visibility status and which templates welcomebar displays."
+      >
+        <Card sectioned>
+          <FormLayout>
+            <Checkbox
+              label="Active"
+              helpText="Live for public view"
+              checked={isActive}
+              id="isActive"
+              onChange={updateFieldValue}
+            />
+            <ChoiceList
+              title="Page visibility"
+              choices={[
+                { label: 'Global (all pages)', value: 'global' },
+                { label: 'Homepage', value: 'homepage' },
+                { label: 'Cart', value: 'cart' },
+                { label: 'Collections', value: 'collection' },
+                { label: 'Products', value: 'product' },
+              ]}
+              selected={[pageTemplate]}
+              name="pageTemplate"
+              onChange={updateFieldValue}
+              allowMultiple
+            />
+          </FormLayout>
+        </Card>
+      </Layout.AnnotatedSection>
+      <Layout.AnnotatedSection title="Link" description="Settings for welcome bar link.">
+        <Card sectioned>
+          <FormLayout>
+            <TextField
+              value={url}
+              label="URL"
+              id="url"
+              placeholder="https://example.com"
+              onChange={updateFieldValue}
+            />
+            <Checkbox
+              label="Open link in new tab"
+              id="isNewTabUrl"
+              checked={isNewTabUrl}
+              onChange={updateFieldValue}
+            />
+            <Checkbox
+              label="Entire bar is link"
+              id="isFullWidthLink"
+              checked={isFullWidthLink}
+              onChange={updateFieldValue}
+            />
+          </FormLayout>
+        </Card>
+      </Layout.AnnotatedSection>
+      <Layout.AnnotatedSection title="Display styles" description="Style the overall display.">
+        <Card sectioned>
+          <FormLayout>
+            <ChoiceList
+              title="Placement"
+              choices={[{ label: 'Top', value: 'top' }, { label: 'Bottom', value: 'bottom' }]}
+              selected={[placement]}
+              name="placement"
+              onChange={updateFieldValue}
+            />
+            <Checkbox
+              label="Is sticky bar"
+              helpText="Bar sticks to top/bottom on scroll"
+              checked={isSticky}
+              id="isSticky"
+              onChange={updateFieldValue}
+            />
+            <RangeSlider
+              label="Vertical padding"
+              value={paddingY}
+              id="paddingY"
+              onChange={updateFieldValue}
+            />
+            <RangeSlider
+              label="Horizontal padding"
+              value={paddingX}
+              id="paddingX"
+              onChange={updateFieldValue}
+            />
+            <Checkbox
+              label="Show close button"
+              checked={hasCloseButton}
+              id="hasCloseButton"
+              onChange={updateFieldValue}
+            />
+          </FormLayout>
+        </Card>
+      </Layout.AnnotatedSection>
+      <Layout.AnnotatedSection title="Text styles" description="Style the welcome bar text.">
+        <Card sectioned>
+          <FormLayout>
+            Text color
+            <ColorPicker
+              allowAlpha
+              id="fontColor"
+              color={{ hue: 120, brightness: 1, saturation: 1 }} // Temporary
+              // color={fontColor}
+              onChange={updateColorPickerValue}
+            />
+            <ChoiceList
+              title="Alignment"
+              choices={[
+                { label: 'Left', value: 'left' },
+                { label: 'Center', value: 'center' },
+                { label: 'Right', value: 'right' },
+              ]}
+              name="textAlign"
+              selected={[textAlign]}
+              onChange={updateFieldValue}
+            />
+            <ChoiceList
+              title="Inherit font size"
+              choices={[
+                { label: 'Use default size from page', value: true },
+                { label: 'Set custom font size', value: false },
+              ]}
+              name="fontSize"
+              selected={[fontSize]} // @TODO: Set conditional field
+              onChange={updateFieldValue}
+            />
+            <RangeSlider
+              label="Size"
+              value={fontSize}
+              id="fontSize"
+              onChange={updateFieldWithPixel}
+            />
+          </FormLayout>
+        </Card>
+      </Layout.AnnotatedSection>
+      <Layout.AnnotatedSection
+        title="Background styles"
+        description="Style the background and or upload an image."
+      >
+        <Card sectioned>
+          <FormLayout>
+            Background color
+            <ColorPicker
+              allowAlpha
+              id="backgroundColor"
+              color={{ hue: 120, brightness: 1, saturation: 1 }} // Temporary
+              // color={backgroundColor}
+              onChange={updateColorPickerValue}
+            />
+            <ChoiceList
+              title="Image position vertical placement"
+              choices={[
+                { label: 'Center', value: 'center' },
+                { label: 'Use custom number', value: false }, // @TODO: Set conditional field
+              ]}
+              name="backgroundImagePositionY"
+              selected={[backgroundImagePositionY]}
+              onChange={updateFieldValue}
+            />
+            <ChoiceList
+              title="Image position horizontal placement"
+              choices={[
+                { label: 'Center', value: 'center' },
+                { label: 'Use custom number', value: false }, // @TODO: Set conditional field
+              ]}
+              name="backgroundImagePositionX"
+              selected={[backgroundImagePositionX]}
+              onChange={updateFieldValue}
+            />
+            <ChoiceList
+              title="Image vertical size"
+              choices={[
+                { label: 'Default (auto size)', value: 'auto' },
+                { label: 'Contained image', value: 'contain' },
+                { label: 'Fit entire container', value: 'cover' },
+                { label: 'Use custom number', value: false }, // @TODO: Set conditional field
+                { label: 'Use custom percent', value: 'backgroundSizeYPixel' }, // @TODO: Set conditional field
+              ]}
+              name="backgroundImageSizeY"
+              selected={[backgroundImageSizeY]}
+              onChange={updateFieldValue}
+            />
+            <ChoiceList
+              title="Image horizontal size"
+              choices={[
+                { label: 'Default (auto size)', value: 'auto' },
+                { label: 'Contained image', value: 'contain' },
+                { label: 'Fit entire container', value: 'cover' },
+                { label: 'Use custom number', value: false }, // @TODO: Set conditional field
+                { label: 'Use custom percent', value: 'backgroundSizeXPercent' }, // @TODO: Set conditional field
+              ]}
+              name="backgroundImageSizeX"
+              selected={[backgroundImageSizeX]}
+              onChange={updateFieldValue}
+            />
+            <ChoiceList
+              title="Image repeat"
+              choices={[
+                { label: 'Do not repeat', value: 'no-repeat' },
+                { label: 'Repeat', value: 'repeat' },
+                { label: 'Repeat vertically only', value: 'repeat-y' },
+                { label: 'Repeat horizontally only', value: 'repeat-x' },
+              ]}
+              name="backgroundImageRepeat"
+              selected={[backgroundImageRepeat]}
+              onChange={updateFieldValue}
+            />
+          </FormLayout>
+        </Card>
+      </Layout.AnnotatedSection>
+    </Layout>
+  );
+};
+
+SingleBarFormFields.propTypes = {
+  fieldValues: PropTypes.instanceOf(Object),
+  updateFieldValue: PropTypes.func.isRequired,
+  updateFieldWithPixel: PropTypes.func.isRequired,
+  updateColorPickerValue: PropTypes.func.isRequired,
+};
+
+SingleBarFormFields.defaultProps = {
+  fieldValues: {},
+};
+
+export default SingleBarFormFields;
--- client/pages/single-bar.js
+++ client/pages/single-bar.js
@@ -1,19 +1,11 @@
-/* eslint react/prefer-stateless-function: 0 */
-
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Layout, Page, Card, Form, TextField, FormLayout, ChoiceList, RangeSlider, ColorPicker, Checkbox } from '@shopify/polaris';
-import '@shopify/polaris/styles.css';
+import { Page, Form } from '@shopify/polaris';
+import SingleBarFormFields from '../components/SingleBarFormFields';
 
 class SingleBar extends React.Component {
   static propTypes = {
-    bar: PropTypes.shape({
-      id: PropTypes.number,
-      title: PropTypes.string,
-      content: PropTypes.string,
-      hasCloseButton: PropTypes.bool,
-      isActive: PropTypes.bool,
-    }),
+    bar: PropTypes.instanceOf(Object),
   };
 
   static defaultProps = {
@@ -46,11 +38,15 @@ class SingleBar extends React.Component {
       backgroundImage: '',
       backgroundImageRepeat: '',
       backgroundImageSizeX: '',
+      backgroundImageSizeY: '',
       backgroundImagePositionX: '',
       backgroundImagePositionY: '',
     };
 
+    this.handleFormSubmit = this.handleFormSubmit.bind(this);
     this.handleValueChange = this.handleValueChange.bind(this);
+    this.handleColorPickerValueChange = this.handleColorPickerValueChange.bind(this);
+    this.handlePixelValueChange = this.handlePixelValueChange.bind(this);
   }
 
   componentDidMount() {
@@ -58,45 +54,34 @@ class SingleBar extends React.Component {
     const { ...state } = this.state;
 
     const nextState = {};
-    Object.keys(bar).forEach((key) => {
-      nextState[key] = bar[key];
-    });
+    Object.keys(bar)
+      .filter((key) => bar[key])
+      .forEach((key) => {
+        nextState[key] = bar[key];
+      });
 
-    this.setState({ ...state, nextState });
+    this.setState({ ...state, ...nextState });
   }
 
-  handleValueChange(field) {
-    return (value) => this.setState({ [field]: value });
+  handleFormSubmit(e) {
+    e.preventDefault();
+
+    const { ...state } = this.state;
+  }
+
+  handleValueChange(value, id) {
+    this.setState({ [id]: value });
+  }
+
+  handleColorPickerValueChange(field) {
+    // this.handleValueChange(field);
+  }
+
+  handlePixelValueChange(field) {
+    this.handlePixelValueChange(field);
   }
 
   render() {
-    const {
-      title,
-      content,
-      hasCloseButton,
-      isActive,
-      placement,
-      pageTemplate,
-      isSticky,
-      url,
-      isFullWidthLink,
-      isNewTabUrl,
-      paddingY,
-      paddingX,
-      fontSize,
-      fontColor,
-      fontFamily, // @TODO: Google fonts select?
-      textOpacity,
-      textAlign,
-      backgroundColor,
-      backgroundOpacity,
-      backgroundImage, // @TODO: Image uploader
-      backgroundImageRepeat,
-      backgroundImageSizeY,
-      backgroundImageSizeX,
-      backgroundImagePositionX,
-      backgroundImagePositionY,
-    } = this.state;
     const { bar } = this.props;
     const primaryAction = {
       content: 'Save',
@@ -104,226 +89,13 @@ class SingleBar extends React.Component {
 
     return (
       <Page title={bar.title} forceRender primaryAction={primaryAction}>
-        <Form>
-          <Layout>
-            <Layout.AnnotatedSection
-              title="Editor"
-              description="A sample form using Polaris components."
-            >
-              <Card sectioned>
-                <FormLayout>
-                  <TextField
-                    value={title}
-                    label="Title"
-                    placeholder="Title"
-                    onChange={this.handleValueChange('title')}
-                  />
-                  <TextField
-                    value={content}
-                    label="Content"
-                    placeholder="Start typing..."
-                    onChange={this.handleValueChange('content')}
-                    multiline
-                  />
-                </FormLayout>
-              </Card>
-            </Layout.AnnotatedSection>
-            <Layout.AnnotatedSection
-              title="Visibility"
-              description="A sample form using Polaris components."
-            >
-              <Card sectioned>
-                <FormLayout>
-                  <ChoiceList
-                    title="Active status"
-                    choices={[
-                      { label: 'Active', value: true },
-                      { label: 'Inactive', value: false },
-                    ]}
-                    selected={[isActive]}
-                    onChange={this.handleValueChange('isActive')}
-                  />
-                  <ChoiceList
-                    title="Page visibility"
-                    choices={[
-                      { label: 'Global (all pages)', value: 'global' },
-                      { label: 'Homepage', value: 'homepage' },
-                      { label: 'Cart', value: 'cart' },
-                      { label: 'Collections', value: 'collection' },
-                      { label: 'Products', value: 'product' },
-                    ]}
-                    selected={[pageTemplate]}
-                    onChange={this.handleValueChange('pageTemplate')}
-                    allowMultiple
-                  />
-                </FormLayout>
-              </Card>
-            </Layout.AnnotatedSection>
-            <Layout.AnnotatedSection
-              title="Link"
-              description="A sample form using Polaris components."
-            >
-              <Card sectioned>
-                <FormLayout>
-                  <TextField
-                    value={url}
-                    label="URL"
-                    placeholder="https://example.com"
-                    onChange={this.handleValueChange('title')}
-                  />
-                  <Checkbox
-                    label="Open link in new tab"
-                    checked={[isNewTabUrl]}
-                    onChange={this.handleValueChange('isNewTabUrl')}
-                  />
-                  <Checkbox
-                    label="Entire bar is link"
-                    checked={[isFullWidthLink]}
-                    onChange={this.handleValueChange('isFullWidthLink')}
-                  />
-                </FormLayout>
-              </Card>
-            </Layout.AnnotatedSection>
-            <Layout.AnnotatedSection
-              title="Display styles"
-              description="A sample form using Polaris components."
-            >
-              <Card sectioned>
-                <FormLayout>
-                  <ChoiceList
-                    title="Placement"
-                    choices={[{ label: 'Top', value: 'top' }, { label: 'Bottom', value: 'bottom' }]}
-                    selected={[placement]}
-                    onChange={this.handleValueChange('placement')}
-                  />
-                  <ChoiceList
-                    title="Sticky bar"
-                    choices={[
-                      { label: 'Sticky position', value: true },
-                      { label: 'Static position', value: false },
-                    ]}
-                    selected={[isSticky]}
-                    onChange={this.handleValueChange('isSticky')}
-                  />
-                  <RangeSlider
-                    label="Vertical padding"
-                    value={paddingY}
-                    onChange={this.handleValueChange('paddingY')}
-                  />
-                  <RangeSlider
-                    label="Horizontal padding"
-                    value={paddingX}
-                    onChange={this.handleValueChange('paddingX')}
-                  />
-                  <ChoiceList
-                    title="Close button"
-                    choices={[{ label: 'Show', value: true }, { label: 'Hide', value: false }]}
-                    selected={[hasCloseButton]}
-                    onChange={this.handleValueChange('hasCloseButton')}
-                  />
-                </FormLayout>
-              </Card>
-            </Layout.AnnotatedSection>
-            <Layout.AnnotatedSection
-              title="Text styles"
-              description="A sample form using Polaris components."
-            >
-              <Card sectioned>
-                <FormLayout>
-                  Text color
-                  <ColorPicker color={fontColor} allowAlpha />
-                  <ChoiceList
-                    title="Alignment"
-                    choices={[
-                      { label: 'Left', value: 'left' },
-                      { label: 'Center', value: 'center' },
-                      { label: 'Right', value: 'right' },
-                    ]}
-                    selected={[textAlign]}
-                    onChange={this.handleValueChange('textAlign')}
-                  />
-                  <ChoiceList
-                    title="Inherit font size"
-                    choices={[
-                      { label: 'Use default size from page', value: true },
-                      { label: 'Set custom font size', value: false },
-                    ]}
-                    selected={[fontSize]} // @TODO: Change this to hide/show field below
-                    onChange={this.handleValueChange('fontSize')}
-                  />
-                  <RangeSlider
-                    label="Size"
-                    value={fontSize}
-                    onChange={this.handleValueChange('fontSize')}
-                  />
-                </FormLayout>
-              </Card>
-            </Layout.AnnotatedSection>
-            <Layout.AnnotatedSection
-              title="Background styles"
-              description="A sample form using Polaris components."
-            >
-              <Card sectioned>
-                <FormLayout>
-                  Background color
-                  <ColorPicker color={backgroundColor} allowAlpha />
-                  <ChoiceList
-                    title="Image position vertical placement"
-                    choices={[
-                      { label: 'Center', value: 'center' },
-                      { label: 'Use custom number', value: false }, // @TODO: Set conditional field
-                    ]}
-                    selected={[backgroundImagePositionY]}
-                    onChange={this.handleValueChange('backgroundImagePositionY')}
-                  />
-                  <ChoiceList
-                    title="Image position horizontal placement"
-                    choices={[
-                      { label: 'Center', value: 'center' },
-                      { label: 'Use custom number', value: false }, // @TODO: Set conditional field
-                    ]}
-                    selected={[backgroundImagePositionX]}
-                    onChange={this.handleValueChange('backgroundImagePositionX')}
-                  />
-                  <ChoiceList
-                    title="Image vertical size"
-                    choices={[
-                      { label: 'Default (auto size)', value: 'auto' },
-                      { label: 'Contained image', value: 'contain' },
-                      { label: 'Fit entire container', value: 'cover' },
-                      { label: 'Use custom number', value: false }, // @TODO: Set conditional field
-                      { label: 'Use custom percent', value: 'backgroundSizeYPixel' }, // @TODO: Set conditional field
-                    ]}
-                    selected={[backgroundImageSizeY]}
-                    onChange={this.handleValueChange('backgroundImageSizeY')}
-                  />
-                  <ChoiceList
-                    title="Image horizontal size"
-                    choices={[
-                      { label: 'Default (auto size)', value: 'auto' },
-                      { label: 'Contained image', value: 'contain' },
-                      { label: 'Fit entire container', value: 'cover' },
-                      { label: 'Use custom number', value: false }, // @TODO: Set conditional field
-                      { label: 'Use custom percent', value: 'backgroundSizeXPercent' }, // @TODO: Set conditional field
-                    ]}
-                    selected={[backgroundImageSizeX]}
-                    onChange={this.handleValueChange('backgroundImageSizeX')}
-                  />
-                  <ChoiceList
-                    title="Image repeat"
-                    choices={[
-                      { label: 'Do not repeat', value: 'no-repeat' },
-                      { label: 'Repeat', value: 'repeat' },
-                      { label: 'Repeat vertically only', value: 'repeat-y' },
-                      { label: 'Repeat horizontally only', value: 'repeat-x' },
-                    ]}
-                    selected={[backgroundImageRepeat]}
-                    onChange={this.handleValueChange('backgroundImageRepeat')}
-                  />
-                </FormLayout>
-              </Card>
-            </Layout.AnnotatedSection>
-          </Layout>
+        <Form onSubmit={this.handleFormSubmit}>
+          <SingleBarFormFields
+            updateFieldValue={this.handleValueChange}
+            updateFieldWithPixel={this.handlePixelValueChange}
+            updateColorPickerValue={this.handleColorPickerValueChange}
+            {...this.state}
+          />
         </Form>
       </Page>
     );
