a6aa3234e657eb8a02625d8185c73323e3949786
Julian <hello@juliantrueflynn.com>
2019-02-06 13:34:23 -0500

    Setup auth routes

--- client/server/app.js
+++ client/server/app.js
@@ -1,5 +1,5 @@
 const Koa = require('koa');
-const next = require('next');
+const Next = require('next');
 const session = require('koa-session');
 const Router = require('koa-router');
 const { default: createShopifyAuth, verifyRequest } = require('@shopify/koa-shopify-auth');
@@ -22,7 +22,7 @@ const {
 const dev = NODE_ENV !== 'production';
 const port = parseInt(PORT, 10) || 3000;
 
-const app = next({ dev });
+const app = Next({ dev });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
@@ -30,8 +30,47 @@ app.prepare().then(() => {
   const router = new Router();
 
   server.keys = [SHOPIFY_API_SECRET_KEY];
+  server.use(c2k(proxy('/api', { target: API_URL })));
 
-  server.use(session(server));
+  router.use(session(server));
+
+  router.use(
+    '/auth',
+    createShopifyAuth({
+      apiKey: SHOPIFY_API_CLIENT_KEY,
+      secret: SHOPIFY_API_SECRET_KEY,
+      scopes: ['write_script_tags'],
+      afterAuth(ctx) {
+        const { shop, accessToken } = ctx.session;
+        ctx.cookies.set('shopOrigin', shop, { httpOnly: false });
+
+        const scriptTagBody = JSON.stringify({
+          script_tag: {
+            event: 'onload',
+            src: `${TUNNEL_URL}/static/js/welcomeBar.js`,
+          },
+        });
+
+        const addScriptTagOptions = {
+          method: 'POST',
+          body: scriptTagBody,
+          headers: {
+            'X-Shopify-Access-Token': accessToken,
+            'Content-Type': 'application/json',
+            Accept: '*/*',
+          },
+          json: true,
+        };
+
+        const scriptTagApiUrl = `https://${shop}/admin/script_tags.json`;
+
+        fetch(scriptTagApiUrl, addScriptTagOptions);
+
+        ctx.redirect('/');
+      },
+    }),
+    verifyRequest(),
+  );
 
   router.get('/bars/:id', async (ctx) => {
     const query = Object.assign({}, ctx.query, ctx.params);
@@ -39,57 +78,23 @@ app.prepare().then(() => {
     ctx.respond = false;
   });
 
-  server
-    .use(c2k(proxy('/api', { target: API_URL })))
-    .use(
-      createShopifyAuth({
-        apiKey: SHOPIFY_API_CLIENT_KEY,
-        secret: SHOPIFY_API_SECRET_KEY,
-        scopes: ['write_script_tags'],
-        async afterAuth(ctx) {
-          const { shop, accessToken } = ctx.session;
-          ctx.cookies.set('shopOrigin', shop, { httpOnly: false });
+  router.get('*', async (ctx) => {
+    await handle(ctx.req, ctx.res);
+    ctx.respond = false;
+  });
 
-          const scriptTagBody = JSON.stringify({
-            script_tag: {
-              event: 'onload',
-              src: `${TUNNEL_URL}/static/js/welcomeBar.js`,
-            },
-          });
+  server.use(async (ctx, next) => {
+    ctx.res.statusCode = 200;
+    await next();
+  });
 
-          const addScriptTagOptions = {
-            method: 'POST',
-            credentials: 'include',
-            body: scriptTagBody,
-            headers: {
-              'X-Shopify-Access-Token': accessToken,
-              'Content-Type': 'application/json',
-              Accept: '*/*',
-            },
-            json: true,
-          };
+  server.use(router.routes());
 
-          const scriptTagApiUrl = `https://${shop}/admin/script_tags.json`;
+  server.listen(port, (err) => {
+    if (err) {
+      throw err;
+    }
 
-          await fetch(scriptTagApiUrl, addScriptTagOptions).then((response) => response.json());
-
-          ctx.redirect('/');
-        },
-      }),
-    )
-    .use(verifyRequest())
-    .use(router.routes())
-    .use(async (ctx) => {
-      await handle(ctx.req, ctx.res);
-      ctx.respond = false;
-      ctx.res.statusCode = 200;
-      return false;
-    })
-    .listen(port, (err) => {
-      if (err) {
-        throw err;
-      }
-
-      console.log(`> Ready on ${TUNNEL_URL}, PORT: ${port}`); // eslint-disable-line no-console
-    });
+    console.log(`> Ready on ${TUNNEL_URL}, PORT: ${port}`); // eslint-disable-line no-console
+  });
 });
