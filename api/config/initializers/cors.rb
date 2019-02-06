46167ae8d44d38000d6cd84245343c7a5f10372c
Julian <hello@juliantrueflynn.com>
2019-02-06 11:35:10 -0500

    Whitelist any origin for active_bars api endpoint

--- api/config/initializers/cors.rb
+++ api/config/initializers/cors.rb
@@ -1,5 +1,10 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
+    origins '*'
+    resource '/api/active_bars/*', headers: :any, methods: [:get, :options, :head]
+  end
+
+  allow do
     origins ENV['CORS_ORIGINS'].split(',').map { |origin| origin.strip }
 
     resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
