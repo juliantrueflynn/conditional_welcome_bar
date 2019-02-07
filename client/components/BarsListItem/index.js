378e6f4baea1b0c2e2bc374aef0f62b3bd787e61
Julian <hello@juliantrueflynn.com>
2019-02-07 16:32:00 -0500

    Add event handler for onClick

--- client/components/BarsListItem/index.js
+++ client/components/BarsListItem/index.js
@@ -1,34 +1,53 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Router from 'next/router';
 import { ResourceList, TextStyle } from '@shopify/polaris';
 import TimeAgo from 'timeago-react';
 import './styles.css';
 
-const BarsListItem = (props) => {
-  const { id, title, content, createdAt } = props;
-  const createDatetime = new Date(createdAt);
-  const accessibilityLabel = `View details for ${title}`;
-  const url = { to: { pathname: '/single-bar', query: { id } }, as: `/bars/${id}` };
+class BarsListItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleOnClick = this.handleOnClick.bind(this);
+  }
 
-  return (
-    <div className="BarsListItem">
-      <ResourceList.Item id={id} url={url} accessibilityLabel={accessibilityLabel}>
-        <div className="BarsListItem__Main">
-          <h3 className="BarsListItem__Title">
-            <TextStyle variation="strong">{title}</TextStyle>
-          </h3>
-          <div className="BarsListItem__Date">
-            <TextStyle variation="subdued">
-              {'Created '}
-              <TimeAgo datetime={createDatetime} />
-            </TextStyle>
+  handleOnClick() {
+    const { id } = this.props;
+    const pathname = '/single-bar';
+    const query = { id };
+
+    Router.push({ pathname, query }, `/bars/${id}`);
+  }
+
+  render() {
+    const { id, title, content, createdAt } = this.props;
+    const createDatetime = new Date(createdAt);
+    const accessibilityLabel = `View details for ${title}`;
+
+    return (
+      <div className="BarsListItem">
+        <ResourceList.Item
+          id={id}
+          onClick={this.handleOnClick}
+          accessibilityLabel={accessibilityLabel}
+        >
+          <div className="BarsListItem__Main">
+            <h3 className="BarsListItem__Title">
+              <TextStyle variation="strong">{title}</TextStyle>
+            </h3>
+            <div className="BarsListItem__Date">
+              <TextStyle variation="subdued">
+                {'Created '}
+                <TimeAgo datetime={createDatetime} />
+              </TextStyle>
+            </div>
           </div>
-        </div>
-        {content}
-      </ResourceList.Item>
-    </div>
-  );
-};
+          {content}
+        </ResourceList.Item>
+      </div>
+    );
+  }
+}
 
 BarsListItem.propTypes = {
   id: PropTypes.number.isRequired,
