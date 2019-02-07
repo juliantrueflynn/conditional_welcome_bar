0e572aa010ff300213b67a4e792c985003a3b4c8
Julian <hello@juliantrueflynn.com>
2019-02-06 20:42:16 -0500

    Cleanup before ticket

--- client/server/app.js
+++ client/server/app.js
@@ -5,6 +5,7 @@ const Router = require('koa-router');
 const { default: createShopifyAuth, verifyRequest } = require('@shopify/koa-shopify-auth');
 const dotenv = require('dotenv');
 const proxy = require('http-proxy-middleware');
+const bodyParser = require('koa-bodyparser');
 const c2k = require('koa2-connect');
 require('isomorphic-fetch');
 
@@ -25,52 +26,50 @@ const port = parseInt(PORT, 10) || 3000;
 const app = Next({ dev });
 const handle = app.getRequestHandler();
 
+const shopifyConfig = {
+  apiKey: SHOPIFY_API_CLIENT_KEY,
+  secret: SHOPIFY_API_SECRET_KEY,
+  scopes: ['write_script_tags'],
+  afterAuth(ctx) {
+    const { shop, accessToken } = ctx.session;
+    ctx.cookies.set('shopOrigin', shop, { httpOnly: false });
+
+    const scriptTagBody = JSON.stringify({
+      script_tag: {
+        event: 'onload',
+        src: `${TUNNEL_URL}/static/js/welcomeBar.js`,
+      },
+    });
+
+    const addScriptTagOptions = {
+      method: 'POST',
+      body: scriptTagBody,
+      headers: {
+        'X-Shopify-Access-Token': accessToken,
+        'Content-Type': 'application/json',
+        Accept: '*/*',
+      },
+      json: true,
+    };
+
+    const scriptTagApiUrl = `https://${shop}/admin/script_tags.json`;
+
+    fetch(scriptTagApiUrl, addScriptTagOptions);
+
+    ctx.redirect('/');
+  },
+};
+
 app.prepare().then(() => {
   const server = new Koa();
   const router = new Router();
 
+  server.use(session(server));
   server.keys = [SHOPIFY_API_SECRET_KEY];
+  server.use(bodyParser());
   server.use(c2k(proxy('/api', { target: API_URL })));
 
-  router.use(session(server));
-
-  router.use(
-    '/auth',
-    createShopifyAuth({
-      apiKey: SHOPIFY_API_CLIENT_KEY,
-      secret: SHOPIFY_API_SECRET_KEY,
-      scopes: ['write_script_tags'],
-      afterAuth(ctx) {
-        const { shop, accessToken } = ctx.session;
-        ctx.cookies.set('shopOrigin', shop, { httpOnly: false });
-
-        const scriptTagBody = JSON.stringify({
-          script_tag: {
-            event: 'onload',
-            src: `${TUNNEL_URL}/static/js/welcomeBar.js`,
-          },
-        });
-
-        const addScriptTagOptions = {
-          method: 'POST',
-          body: scriptTagBody,
-          headers: {
-            'X-Shopify-Access-Token': accessToken,
-            'Content-Type': 'application/json',
-            Accept: '*/*',
-          },
-          json: true,
-        };
-
-        const scriptTagApiUrl = `https://${shop}/admin/script_tags.json`;
-
-        fetch(scriptTagApiUrl, addScriptTagOptions);
-
-        ctx.redirect('/');
-      },
-    }),
-    verifyRequest(),
-  );
+  router.use('/auth', createShopifyAuth(shopifyConfig), verifyRequest());
 
   router.get('/bars/:id', async (ctx) => {
     const query = Object.assign({}, ctx.query, ctx.params);
