e1197795ced599bc0e540a57c2e25781d353d5e2
Julian <hello@juliantrueflynn.com>
2019-02-13 14:36:01 -0500

    Add necessary utils from next.js

---
+++ client/util/apiUtil.js
@@ -0,0 +1,40 @@
+import fetch from 'isomorphic-fetch';
+import { decamelizeKeys } from 'humps';
+
+// eslint-disable-next-line no-undef
+const endpoint = (url) => `${TUNNEL_URL}/api/${url}`;
+
+const fetchPromise = (url, args) =>
+  fetch(endpoint(url), { credentials: 'include', ...args })
+    .then((response) => response.json().then((json) => ({ json, response })))
+    .then(({ json, response }) => {
+      if (!response.ok) {
+        throw json;
+      }
+
+      return json;
+    })
+    .catch((error) => {
+      throw error || ['Unknown error!'];
+    });
+
+export const apiCall = (method, url, props) => {
+  const args = {
+    method,
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+  };
+
+  if (args.method === 'POST' || args.method === 'PATCH') {
+    args.body = JSON.stringify(decamelizeKeys(props, { separator: '_' }));
+  }
+
+  return fetchPromise(url, args);
+};
+
+export const apiFetch = (url, props = {}) => apiCall('GET', url, props);
+export const apiCreate = (url, props = {}) => apiCall('POST', url, props);
+export const apiUpdate = (url, props = {}) => apiCall('PATCH', url, props);
+export const apiDestroy = (url) => fetchPromise(url, { method: 'DELETE' });
