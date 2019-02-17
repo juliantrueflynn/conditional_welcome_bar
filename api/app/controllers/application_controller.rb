db163d192097d7147085b3e8deeeeb1a0b6a17b9
Julian <hello@juliantrueflynn.com>
2019-02-16 19:39:42 -0500

    Add jwt auth controller

--- api/app/controllers/application_controller.rb
+++ api/app/controllers/application_controller.rb
@@ -1,3 +1,34 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery unless: -> { request.format.json? }
+
+  before_action :authenticate 
+
+  def logged_in?
+    !!current_shop
+  end
+
+  def current_shop
+    if auth_present?
+      shop = Shop.find(auth['shop'])
+      @current_shop ||= shop if shop
+    end
+  end
+
+  def authenticate
+    render json: { error: 'unauthorized' }, status: 404 unless logged_in?
+  end
+
+  private
+
+  def token
+    request.env['HTTP_AUTHORIZATION'].scan(/Bearer (.*)$/).flatten.last
+  end
+
+  def auth
+    Auth.decode(token)
+  end
+
+  def auth_present?
+    !!request.env.fetch('HTTP_AUTHORIZATION', '').scan(/Bearer/).flatten.first
+  end
 end
