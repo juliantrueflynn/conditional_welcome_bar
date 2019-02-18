56c36439594892b82fd291993e9b61c1af74505b
Julian <hello@juliantrueflynn.com>
2019-02-18 16:22:10 -0500

    Fix shop tests

--- api/spec/requests/shops_spec.rb
+++ api/spec/requests/shops_spec.rb
@@ -2,18 +2,19 @@ require 'rails_helper'
 
 RSpec.describe 'Shops', type: :request do
   describe 'CREATE /shop' do
-    let!(:shop) { FactoryBot.attributes_for(:shop) }
+    let!(:path) { "#{ENV['API_URL']}/api/shop" }
 
     it 'when valid attributes' do
       expect {
-        post "#{ENV['API_URL']}/api/shop", params: { shop: shop }
+        shop = FactoryBot.attributes_for(:shop)
+        post path, params: shop
       }.to change(Shop, :count).by(1)
     end
 
     it 'when not valid attributes' do
       expect {
-        invalid_params = { shop: { title: 'Fail', content: nil } }
-        post "#{ENV['API_URL']}/api/shop", params: invalid_params
+        invalid_params = { shop: { shopify_domain: nil } }
+        post path, params: invalid_params
       }.to_not change(Shop, :count)
     end
   end
