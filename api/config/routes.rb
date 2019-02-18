5b9c92cb56918143fdaa50afcb66243ec6ac5fd1
Julian <hello@juliantrueflynn.com>
2019-02-17 19:50:38 -0500

    Add route for jwt token

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,14 +1,18 @@
 Rails.application.routes.draw do
-  SHOP_CONSTRAINT = { constraints: { shop_domain: /[^\/]+/ } }.freeze
-
   namespace :api, defaults: { format: :json } do
     resources :bars, only: [:show, :update, :destroy]
 
-    get 'active_bars/:shop_domain', to: 'active_bars#index', **SHOP_CONSTRAINT
+    get 'active_bars/:shop_domain',
+      to: 'active_bars#index',
+      constraints: { shop_domain: /[^\/]+/ }
 
-    resources :shops, only: [], param: :domain, **SHOP_CONSTRAINT do
-      resources :bars, only: [:index, :create]
-    end
+    resources :shops,
+      only: [],
+      param: :domain,
+      constraints: { shop_domain: /[^\/]+/ } do
+        resources :bars, only: [:index, :create]
+        resource :session, only: [:show]
+      end
 
     resource :shop, only: :create
   end
