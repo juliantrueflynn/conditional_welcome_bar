19f9da6160de15d8fd87f762e89e2a1c0864a507
Julian <hello@juliantrueflynn.com>
2019-02-18 16:00:44 -0500

    Update bar tests to use jwt

--- api/spec/requests/bars_spec.rb
+++ api/spec/requests/bars_spec.rb
@@ -1,55 +1,60 @@
 require 'rails_helper'
+require 'auth'
 
 RSpec.describe 'Bars', type: :request do
+  let!(:root_path) { "#{ENV['API_URL']}/api" }
+  let!(:shop) { FactoryBot.create(:shop) }
+  let!(:shop_domain) { shop.shopify_domain }
+  let!(:headers) do
+    jwt = Auth.issue({ shop: shop.id })    
+    { HTTP_AUTHORIZATION: "Bearer #{jwt}" }
+  end
+
   describe "GET /bars" do
+    let!(:path) { "#{root_path}/shops/#{shop_domain}/bars" }
+
     it 'responds with 200' do
-      get "#{ENV['API_URL']}/api/shops/jiffywelcomebar/bars"
+      get path, params: {}, headers: headers
       expect(response).to have_http_status(200)
     end
   end
 
   describe "GET /bar/:id" do
-    before do
-      bar = FactoryBot.create(:bar)
-      get "#{ENV['API_URL']}/api/bars/#{bar.id}"
-    end
+    let!(:bar) { FactoryBot.create(:bar, shop_id: shop.id) }
+    let!(:path) { "#{root_path}/bars/#{bar.id}" }
 
     it 'show resource' do
+      get path, params: {}, headers: headers
       expect(response).to have_http_status(200)
     end
   end
 
   describe "CREATE /bar" do
-    let!(:bar) { FactoryBot.attributes_for(:bar) }
-    let!(:shop) { FactoryBot.create(:shop) }
+    let!(:bar) { FactoryBot.attributes_for(:bar, shop_id: shop.id) }
+    let!(:path) { "#{root_path}/shops/#{shop_domain}/bars" }
+    let!(:valid_params) { { bar: bar } }
 
     it 'when valid attributes' do
       expect {
-        post "#{ENV['API_URL']}/api/shops/#{shop.id}/bar", params: { bar: bar }
+        post path, params: valid_params, headers: headers
       }.to change(Bar, :count).by(1)
-    end
-
-    it 'when not valid attributes' do
-      expect {
-        invalid_params = { bar: { title: 'Fail', content: nil } }
-        post "#{ENV['API_URL']}/api/shops/#{shop.id}/bar", params: invalid_params
-      }.to_not change(Bar, :count)
     end
   end
 
   describe "UPDATE /bar/:id" do
-    let!(:attributes) { { title: 'Updated' } }
+    let!(:path) { "#{root_path}/bars/#{bar.id}" }
+    let!(:valid_params) { { title: 'Updated' } }
+    let!(:invalid_params) { { title: '' } }
     let!(:bar) { FactoryBot.create(:bar) }
 
     it 'when valid attributes' do
-      patch "#{ENV['API_URL']}/api/bars/#{bar.id}", params: { bar: attributes }
+      patch path, params: { bar: valid_params }, headers: headers
       bar.reload
-      expect(bar.title).to eq(attributes[:title])
+      expect(bar.title).to eq(valid_params[:title])
     end
 
     it 'when not valid attributes' do
-      invalid_params = { title: '' }
-      patch "#{ENV['API_URL']}/api/bars/#{bar.id}", params: { bar: invalid_params }
+      patch path, params: { bar: invalid_params }, headers: headers
       bar.reload
       expect(bar.title).to_not eq(invalid_params[:title])
     end
@@ -57,10 +62,12 @@ RSpec.describe 'Bars', type: :request do
 
   describe "DELETE /bar/:id" do
     let!(:bar) { FactoryBot.create(:bar) }
+    let!(:path) { "#{root_path}/bars/#{bar.id}" }
 
     it 'change count' do
-      api_url = "#{ENV['API_URL']}/api/bars/#{bar.id}"
-      expect { delete api_url }.to change(Bar, :count).by(-1)
+      expect {
+        delete path, params: {}, headers: headers
+      }.to change(Bar, :count).by(-1)
     end
   end
 end
