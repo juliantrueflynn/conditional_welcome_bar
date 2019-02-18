8eca080b9d4b617ffee9974f6fb05ff9c34256d4
Julian <hello@juliantrueflynn.com>
2019-02-18 01:56:48 -0500

    Remove unneeded action

--- api/app/controllers/api/active_bars_controller.rb
+++ api/app/controllers/api/active_bars_controller.rb
@@ -1,6 +1,4 @@
 class Api::ActiveBarsController < ApplicationController
-  skip_before_action :authenticate
-
   def index
     @active_bars = Bar.with_active.with_shopify_domain(params[:shop_domain])
   end
