5dbfb8564241c15731ba165097afe0aea9ef8b4b
Julian <hello@juliantrueflynn.com>
2019-05-02 00:37:04 -0400

    Update deps and whitelist cors localhost not in production env

--- api/config/initializers/cors.rb
+++ api/config/initializers/cors.rb
@@ -5,7 +5,9 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   end
 
   allow do
-    origins ENV['CORS_ORIGINS'].split(',').map { |origin| origin.strip }
+    origins = ENV['CORS_ORIGINS']
+    origins = 'http://localhost:3001' unless Rails.env.production?
+    origins origins
 
     resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
