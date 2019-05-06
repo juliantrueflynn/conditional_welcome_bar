5c7e6d07bf0d6383f8db074f9fd5f09c4c41bc2d
Julian <hello@juliantrueflynn.com>
2019-05-06 00:54:53 -0400

    Ignore whiny requests from Ngrok

--- api/config/environments/development.rb
+++ api/config/environments/development.rb
@@ -51,4 +51,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # Used to ignore "Cannot render console from" from NGROK implementation
+  config.web_console.whiny_requests = false
 end
