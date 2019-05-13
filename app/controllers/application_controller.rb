964e30afc2c1e61e41ddd2266ae3832037526ef6
Julian <hello@juliantrueflynn.com>
2019-05-13 18:31:27 -0400

    Add csrf token to cookies for GraphQL

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery unless: -> { request.format.json? }
 
+  after_action :set_csrf_cookie
+
+  def set_csrf_cookie
+    cookies['cwb_csrf'] = form_authenticity_token
+  end
+
   def fallback_index_html
     response.headers.except!('X-Frame-Options')
     render :file => 'public/index.html'
