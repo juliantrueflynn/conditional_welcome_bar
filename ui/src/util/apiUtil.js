98f433810d71da970e57eba0753f1b74b0ea8e22
Julian <hello@juliantrueflynn.com>
2019-05-16 00:44:45 -0400

    Remove unnecessary API functions

--- ui/src/util/apiUtil.js
+++ ui/src/util/apiUtil.js
@@ -1,8 +1,11 @@
-import { decamelizeKeys } from 'humps';
+const headers = {
+  Accept: 'application/json',
+  'Content-Type': 'application/json',
+};
 
-const fetchPromise = (urlPath, args) => {
+export const apiFetch = (urlPath) => {
   const endpoint = `${process.env.REACT_APP_HOST_URL}/api/${urlPath}`;
-  const options = { credentials: 'include', ...args };
+  const options = { method: 'GET', headers, credentials: 'include' };
 
   return fetch(endpoint, options)
     .then((response) => response.json().then((json) => ({ json, response })))
@@ -17,25 +20,3 @@ const fetchPromise = (urlPath, args) => {
       throw error || ['Unknown error!'];
     });
 };
-
-const headers = {
-  Accept: 'application/json',
-  'Content-Type': 'application/json',
-};
-
-const apiCall = (method, url, props) => {
-  const args = { method, headers };
-
-  if (args.method === 'POST' || args.method === 'PATCH') {
-    args.body = JSON.stringify(decamelizeKeys(props, { separator: '_' }));
-  }
-
-  return fetchPromise(url, args);
-};
-
-export const apiFetch = (url, props = {}) => apiCall('GET', url, props);
-export const apiCreate = (url, props = {}) => apiCall('POST', url, props);
-export const apiUpdate = (url, props = {}) => apiCall('PATCH', url, props);
-export const apiDestroy = (url, props = {}) => apiCall('DELETE', url, props);
-
-export const apiUpdateBar = (id, body) => fetchPromise(`bars/${id}`, { method: 'PATCH', body });
