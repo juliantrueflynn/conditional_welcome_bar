5f0e6043951a5918dbc9c95a5b0595b0c94b926a
Julian Flynn <hello@juliantrueflynn.com>
2019-07-01 00:00:00 -0400

    Remove React redirect

--- ui/App.js
+++ ui/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
@@ -6,38 +6,11 @@ import IndexBarsView from './views/IndexBarsView';
 import SingleBarView from './views/SingleBarView';
 import ShopifyProvider from './components/ShopifyProvider';
 import MissingPageView from './views/MissingPageView';
-import { shopOrigin } from './util/shopifyUtil';
-import { apiFetch } from './util/apiUtil';
 
 const App = () => {
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    const LOGIN_URL = '/login';
-
-    if (shopOrigin) {
-      apiFetch(`shops/${shopOrigin}`).then((res) => {
-        if (res.status === 302) {
-          window.location.assign(`${LOGIN_URL}?shop=${shopOrigin}`);
-          return;
-        } else if (res.status === 404) {
-          window.location.assign(LOGIN_URL);
-        } else {
-          setIsLoading(false);
-        }
-      });
-    } else {
-      window.location.assign(LOGIN_URL);
-    }
-  }, []);
-
   const client = new ApolloClient({
     uri: process.env.GRAPHQL_API_URL
   });
-
-  if (isLoading) {
-    return null;
-  }
 
   return (
     <ApolloProvider client={client}>
