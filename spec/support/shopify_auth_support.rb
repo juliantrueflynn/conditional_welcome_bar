dfdaae9dee06249a8d702ed3ad024037ec51a38a
Julian Flynn <hello@juliantrueflynn.com>
2020-04-27 02:29:52 -0400

    Always test mode for OmniAuth

--- spec/support/shopify_auth_support.rb
+++ spec/support/shopify_auth_support.rb
@@ -2,7 +2,6 @@
 
 module ShopifyAuthSupport
   def login(shop)
-    OmniAuth.config.test_mode = true
     OmniAuth.config.add_mock(:shopify, auth_mock_attributes(shop))
 
     auth = OmniAuth.config.mock_auth[:shopify]
