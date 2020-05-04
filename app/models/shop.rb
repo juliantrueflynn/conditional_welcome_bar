749a481c23fe9ded4540e162878aa785cfc8d445
Julian Flynn <hello@juliantrueflynn.com>
2020-05-03 21:55:09 -0400

    Update Shopify gem and deps

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Shop < ActiveRecord::Base
-  include ShopifyApp::SessionStorage
+  include ShopifyApp::ShopSessionStorage
 
   has_many :bars, dependent: :delete_all
 
