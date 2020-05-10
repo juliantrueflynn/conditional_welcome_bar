9f2722753ab189c460646dbf0709b3d93909e58f
Julian Flynn <hello@juliantrueflynn.com>
2020-05-10 16:54:56 -0400

    Setup webpack bundler analyzer script

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -2,4 +2,16 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'production';
 
 const environment = require('./environment');
 
+if (process.env.BUNDLE_ANALYZE) {
+  const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+
+  environment.plugins.append(
+    'BundleAnalyzer',
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'disabled',
+      generateStatsFile: false,
+    })
+  );
+}
+
 module.exports = environment.toWebpackConfig();
