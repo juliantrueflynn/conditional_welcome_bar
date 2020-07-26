bae481214b8588ab9a236d068769cb299ecae535
Julian Flynn <hello@juliantrueflynn.com>
2020-07-26 02:30:27 -0400

    Fetch polyfill and config (#59)
    * Add fetch polyfill and type checking for dev
    
    * Skip lib check in config to follow fork-ts-checker directions
    
    * Set includes option to limit only to clientside
    
    * Remove packs volume to stop assets breaking
    
    * Cleanup jest config and restrict coverage
    
    * Setup fork-ts-checker with eslint

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -1,5 +1,20 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 const environment = require('./environment');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const path = require('path');
+
+environment.plugins.append(
+  'ForkTsCheckerWebpackPlugin',
+  new ForkTsCheckerWebpackPlugin({
+    typescript: {
+      configFile: path.resolve(__dirname, '../../tsconfig.json'),
+    },
+    eslint: {
+      files: '../../app/javascript/**/*.{ts,tsx,js}',
+    },
+    async: false,
+  })
+);
 
 module.exports = environment.toWebpackConfig();
