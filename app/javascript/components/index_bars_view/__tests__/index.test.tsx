8a3fc7846452ebbd6a3f3aef4b40871bd1bff63b
Julian Flynn <hello@juliantrueflynn.com>
2020-07-26 21:21:37 -0400

    Add network error to index view

--- app/javascript/components/index_bars_view/__tests__/index.test.tsx
+++ app/javascript/components/index_bars_view/__tests__/index.test.tsx
@@ -1,27 +1,37 @@
 import { enableFetchMocks } from 'jest-fetch-mock';
 import React from 'react';
 import IndexBarsView from '..';
-import { render } from '@testing-library/react';
+import { screen, render } from '@testing-library/react';
 import { MockedProvider } from '@apollo/client/testing';
 import { PolarisTestProvider } from '@shopify/polaris';
 import { Router } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
 import { mockBarFields } from '../../../__mocks__/single_bar_mocks';
 import { GET_ALL_BARS } from '../../../utilities/graphql_tags';
+import { GraphQLError } from 'graphql';
 
 const mockBarsData = [
   { ...mockBarFields, id: Math.random(), title: 'Bar #1' },
   { ...mockBarFields, id: Math.random(), title: 'Bar #2' },
 ];
-const graphqlResponseMocks = [
+const graphqlSuccessResponseMocks = [
   {
     request: {
       query: GET_ALL_BARS,
     },
     result: {
-      data: {
-        bars: mockBarsData,
-      },
+      data: { bars: mockBarsData },
+    },
+  },
+];
+const graphqlErrorResponseMocks = [
+  {
+    request: {
+      query: GET_ALL_BARS,
+    },
+    result: {
+      data: { bars: [] },
+      errors: [new GraphQLError('forced network error')],
     },
   },
 ];
@@ -30,7 +40,7 @@ it('renders entries', () => {
   enableFetchMocks();
   const history = createMemoryHistory();
   const { findByText } = render(
-    <MockedProvider mocks={graphqlResponseMocks} addTypename={false}>
+    <MockedProvider mocks={graphqlSuccessResponseMocks} addTypename={false}>
       <Router history={history}>
         <PolarisTestProvider>
           <React.Suspense fallback="Mocked for test">
@@ -44,4 +54,24 @@ it('renders entries', () => {
   mockBarsData.forEach(async (welcomeBar) => {
     expect(await findByText(welcomeBar.title)).toBeInTheDocument();
   });
+});
+
+it('renders error instead of entries', async () => {
+  enableFetchMocks();
+  const history = createMemoryHistory();
+  const { findByText } = render(
+    <MockedProvider mocks={graphqlErrorResponseMocks} addTypename={false}>
+      <Router history={history}>
+        <PolarisTestProvider>
+          <React.Suspense fallback="Mocked for test">
+            <IndexBarsView />
+          </React.Suspense>
+        </PolarisTestProvider>
+      </Router>
+    </MockedProvider>
+  );
+  const errorText = 'Reload this page';
+
+  expect(await findByText(errorText)).toBeInTheDocument();
+  expect(screen.queryByText(mockBarsData[0].title)).not.toBeInTheDocument();
 });
--- app/javascript/components/index_bars_view/index.tsx
+++ app/javascript/components/index_bars_view/index.tsx
@@ -34,7 +34,18 @@ const IndexBarsView: React.FC = () => {
   };
 
   if (error) {
-    return <p>Error :(</p>;
+    return (
+      <EmptyState
+        heading="The page couldn&rsquo;t be displayed due to a network issue."
+        action={{
+          content: 'Reload this page',
+          onAction: () => window.location.reload(),
+        }}
+      >
+        We&rsquo;re either having server issues or there&rsquo;s a problem with
+        your internet connection.
+      </EmptyState>
+    );
   }
 
   return (
