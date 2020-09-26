9cf7a206379800ac43bd534b465345be6546a2aa
Julian Flynn <hello@juliantrueflynn.com>
2020-09-26 01:39:38 -0400

    Centralize env and config for shopify

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -2,12 +2,12 @@
 
 ShopifyApp.configure do |config|
   config.application_name = ENV.fetch("SHOPIFY_APP_NAME", "Conditional Welcome Bar")
-  config.api_key = ENV.fetch("SHOPIFY_API_KEY", nil)
-  config.secret = ENV.fetch("SHOPIFY_API_SECRET", nil)
-  config.scope = ENV.fetch("SHOPIFY_API_SCOPE", "write_script_tags")
+  config.api_key = Rails.configuration.x.shopify.api_key
+  config.secret = Rails.configuration.x.shopify.secret
+  config.scope = Rails.configuration.x.shopify.scope
   config.embedded_app = true
   config.after_authenticate_job = false
-  config.api_version = ENV.fetch("SHOPIFY_API_VERSION", "2020-04")
+  config.api_version = Rails.configuration.x.shopify.api_version
   config.shop_session_repository = "Shop"
   config.enable_same_site_none = Rails.env.production?
   config.scripttags = [{
