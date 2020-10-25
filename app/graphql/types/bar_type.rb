2132593a82781c51161e1135a092f1b086f44d1f
Julian Flynn <hello@juliantrueflynn.com>
2020-10-25 17:14:09 -0400

    Improve non null values from gql

--- app/graphql/types/bar_type.rb
+++ app/graphql/types/bar_type.rb
@@ -3,13 +3,13 @@
 module Types
   class BarType < Types::BaseObject
     field :id, ID, null: false
-    field :title, String, null: true
+    field :title, String, null: false
     field :content, String, null: false
-    field :is_active, Boolean, null: true
-    field :is_sticky, Boolean, null: true
-    field :is_full_width_link, Boolean, null: true
-    field :is_new_tab_url, Boolean, null: true
-    field :has_close_button, Boolean, null: true
+    field :is_active, Boolean, null: false
+    field :is_sticky, Boolean, null: false
+    field :is_full_width_link, Boolean, null: false
+    field :is_new_tab_url, Boolean, null: false
+    field :has_close_button, Boolean, null: false
     field :url, String, null: true
     field :placement, String, null: true
     field :padding_y, String, null: true
@@ -18,8 +18,8 @@ module Types
     field :text_color, String, null: true
     field :font_size, String, null: true
     field :background_color, String, null: true
-    field :created_at, String, null: true
-    field :updated_at, String, null: true
+    field :created_at, String, null: false
+    field :updated_at, String, null: false
     field :theme_templates, [String], null: false
 
     def theme_templates
--- app/graphql/types/user_error_type.rb
+++ app/graphql/types/user_error_type.rb
@@ -4,7 +4,7 @@ module Types
   class UserErrorType < Types::BaseObject
     description "Human-readable errors for attributes on models"
 
-    field :field, [String], null: true, description: "Which input value this error came from"
+    field :field, [String], null: false, description: "Which input value this error came from"
     field :message, String, null: false, description: "A description of the error"
   end
 end
